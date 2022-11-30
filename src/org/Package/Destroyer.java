cce81d4a71019f6e459a31c7b403e81452555df1
eva0supiot <supiot.eva@gmail.com>
2022-11-30 18:22:31 +0100

    Continuation du corps de la Partie

--- src/org/Package/Destroyer.java
+++ src/org/Package/Destroyer.java
@@ -2,8 +2,6 @@ package org.Package;
 import org.Package.Navire;
 
 public class Destroyer extends Navire {
-    private int m_fusee;
-
     public Destroyer(int px, int py, int orientation)
     {
         m_pv=3;
@@ -12,13 +10,10 @@ public class Destroyer extends Navire {
         m_py=py;
         m_orientation=orientation;
         m_puissance=1;
-        m_fusee=1;
+        m_fusee=true;
 
         m_cases=new Case[m_pv];
         for(int k=0;k<m_pv;k++)
             m_cases[k]=new Case();
-    }
-    public void setFusee(int fusee) {
-        this.m_fusee = fusee;
     }
 }
