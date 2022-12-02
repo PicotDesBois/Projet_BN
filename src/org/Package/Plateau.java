9f1e690d40a269c6e9a121d0c0f6fb2b30b6d594
ABmoiu <94547450+ABmoiu@users.noreply.github.com>
2022-12-02 14:45:18 +0100

    Merci à Eva

--- src/org/Package/Plateau.java
+++ src/org/Package/Plateau.java
@@ -1,27 +1,29 @@
 package org.Package;
 import java.util.*;
 
-public class Plateau {
+public class Plateau{
 
     /* en attendant la version graphique j'ai fais une version console*/
 
     private int nbLigne;
     private int nbColonne;
-    private char[][] plateau;
+    private int[][] plateau;
 
-        public Plateau(int n, int p) {
+        public Plateau(int n, int p, Navire[] listeBateaux) {
             nbLigne = n;
             nbColonne = p;
-            plateau = new char[nbLigne][nbColonne];
+            plateau = new int[nbLigne][nbColonne];
 
             for (int i = 0; i < nbLigne; i++) {
                 for (int j = 0; j < nbColonne;j++) {
                     for (int k = 0; k<10; k++){
-                        if (i == Case.(k) && j == (k)){
-                            plateau[i][j] = 1;
-                        }
-                        else {
-                            plateau[i][j] = 0;
+                        for (int r = 0; r   < listeBateaux[k].getPV() ;r++) {
+                            if (i == listeBateaux[k].getCase()[r].getCoorX() && j == listeBateaux[k].getCase()[r].getCoorY()){
+                                plateau[i][j] = k;
+                            }
+                        else{
+                                plateau[i][j] = -1;
+                            }
                         }
                     }
                 }
