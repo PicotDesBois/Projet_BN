2fe9fcd55a601cd59f992135ab38564cef0066f9
Aminebousmah <119043536+Aminebousmah@users.noreply.github.com>
2022-12-11 23:29:46 +0100

    Update GameGUI.java
    On peut pas charger de partie y a quelques bug c'est déjà ça

--- src/Vue/GameGUI.java
+++ src/Vue/GameGUI.java
@@ -2,6 +2,7 @@ package Vue;
 import Controleur.*;
 import Model.*;
 import Model.Case;
+import Model.Navire;
 
 
 import java.awt.event.ActionEvent;
@@ -34,6 +35,19 @@ public class GameGUI extends javax.swing.JFrame {
 
     int DeplacementOn = 0;
     int TireOn = 0;
+
+    int PV0 =0;
+    int PV1 =0;
+    int PV2 =0;
+    int PV3 =0;
+    int PV4 =0;
+    int PV5 =0;
+    int PV6 =0;
+    int PV7 =0;
+    int PV8 =0;
+    int PV9 =0;
+
+
 
 
 
@@ -103,8 +117,8 @@ public class GameGUI extends javax.swing.JFrame {
                             // demander au joueur ou déplacer le navire : haut 1 , bas 2, droite 3, gauche 4
                             // choix entre fusée et tir normal
                             System.out.println("Dans quelle direction voulez-vous vous déplacer ?");
-                            System.out.println("1- vers la droite");
-                            System.out.println("2- vers la gauche");
+                            System.out.println("1- vers la gauche");
+                            System.out.println("2- vers la droite");
                         }
                         // 2 = verticale
                         else if (m_player.getFlotte2(m_choixNavire).getOrientation() == 2) {
@@ -147,17 +161,20 @@ public class GameGUI extends javax.swing.JFrame {
                     }
                     Actualisation();
                     DeplacementOn = 0;
+                    jLabel1.setText("Que voulez-vous faire ?");
                 }
+
+
                 if (TireOn ==1 && sy2 != -1){
-                    m_choixNavire = 5;
+                    m_choixNavire = P2.getCase(sx2,sy2);
 
                     //ChoixCoordTir(m_coor);
                     m_coor = new Case(m_choixNavire,sx2,sy2,true);
                     int [][] cordTir =m_player.getFlotte1()[m_choixNavire].Tirer(m_coor, m_IA.getFlotte1());
-                    g.setColor(Color.BLACK);
+                    /*g.setColor(Color.BLACK);
                     g.drawLine(sx2*26,sy2*26,(sx2+1)*26,(sy2+1)*26);
                     g.setColor(Color.BLACK);
-                    g.drawLine(sx2*26,(sy2+1)*26,(sx2+1)*26,(sy2*26));
+                    g.drawLine(sx2*26,(sy2+1)*26,(sx2+1)*26,(sy2*26));*/
 
 
                     TourIA();
@@ -166,7 +183,8 @@ public class GameGUI extends javax.swing.JFrame {
                         System.out.println("Vous avez perdu");}
                     if (m_victoire == 2){
                         System.out.println("Vous avez gagné");}
-                    //Actualisation();
+                    Actualisation();
+                    jLabel1.setText("Que voulez-vous faire ?");
                     sy2 =-1;
                     TireOn =0;
                 }
@@ -419,8 +437,20 @@ public class GameGUI extends javax.swing.JFrame {
 
 
     }
-
+    Boolean chargement = false;
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {
+
+        fileName = in.nextLine();
+        try {
+            m_player = new Joueur(fileName);
+            m_IA = new Joueur(fileName + "_IA");
+
+            chargement=true;
+        } catch (IOException exc) {
+            jLabel1.setText("Erreur");
+            chargement=false;
+        }
+        Actualisation();
 
 
 
@@ -610,17 +640,16 @@ public class GameGUI extends javax.swing.JFrame {
         aff.AfficherSaisir("l'ordonnée entre A et O");
         coor.setCoorY(saisir.saisirOrdonne());
     }
-
     public void QuiAGagne()
     {
         int nb_IA=0, nb_joueur=0;
 
         for(int i=0;i<10;i++)
         {
-           // if (m_player.getFlotte2(i).m_coule)
-             //   nb_joueur++;
-            //if (m_IA.getFlotte2(i).m_coule)
-              //  nb_IA++;
+           if (m_player.getFlotte2(i).getCoule())
+               nb_joueur++;
+            if (m_IA.getFlotte2(i).getCoule())
+               nb_IA++;
         }
 
         if (nb_joueur==10)
@@ -650,7 +679,6 @@ public class GameGUI extends javax.swing.JFrame {
     public void TourIA(){
         System.out.println("Tour de l'IA");
 
-        // choix de l'action à réaliser
         m_choixAction = (int) (Math.random() * (2 - 1 + 1) + 1);
 
         // choix du navire
@@ -660,7 +688,7 @@ public class GameGUI extends javax.swing.JFrame {
         // choix = tirer
         if (m_choixAction == 1) {
             // si le bateau peut tirer une fusée éclairante
-            if (m_IA.getFlotte2(m_choixNavire).getFusee() == true) {
+            if (m_IA.getFlotte2(m_choixNavire).getFusee()) {
                 // aléatoire entre fusée et tir normal
                 choix_tir = (int) (Math.random() * (2 - 1 + 1) + 1);
 
@@ -670,6 +698,8 @@ public class GameGUI extends javax.swing.JFrame {
                     m_coor.setCoorX((int) (Math.random() * 14 + 1));
                     m_coor.setCoorY((int) (Math.random() * 14 + 1));
                     // tirer la fusée éclairante
+
+
                 }
                 // tir normal
                 else {
@@ -677,7 +707,9 @@ public class GameGUI extends javax.swing.JFrame {
                     m_coor.setCoorX((int) (Math.random() * 14 + 1));
                     m_coor.setCoorY((int) (Math.random() * 14 + 1));
 
-                    m_IA.getFlotte1()[m_choixNavire].Tirer(m_coor, m_player.getFlotte1());
+                    int[][]cordTir = m_IA.getFlotte1()[m_choixNavire].Tirer(m_coor, m_player.getFlotte1());
+
+
                 }
             }
             // tir normal
@@ -686,24 +718,26 @@ public class GameGUI extends javax.swing.JFrame {
                 m_coor.setCoorX((int) (Math.random() * 14 + 1));
                 m_coor.setCoorY((int) (Math.random() * 14 + 1));
 
-                m_IA.getFlotte1()[m_choixNavire].Tirer(m_coor, m_player.getFlotte1());
+                int[][]cordTir = m_IA.getFlotte1()[m_choixNavire].Tirer(m_coor, m_player.getFlotte1());
+
             }
         }
         // choix = déplacer
         else {
             // demander au joueur ou déplacer le navire : haut 1 , bas 2, droite 3, gauche 4
-            choir_dep = (int) (Math.random() * (4 - 1 + 1) + 1);
+            choir_dep = (int) ((Math.random() * (2 - 1 + 1)) + 1);
             do {
                 Deplacement = m_IA.getFlotte2(m_choixNavire).Deplacer(choir_dep, m_IA.getFlotte1(), m_choixNavire);
 
                 if (!Deplacement) {
                     // choix du navire
                     m_choixNavire = (int) (Math.random() * 9 + 1);
-                } else;
+                }
 
             } while (!Deplacement);
         }
-    }
+        }
+
     public void Actualisation(){
 
         P1.PlateauFill(m_player.getFlotte1());
@@ -743,7 +777,7 @@ public class GameGUI extends javax.swing.JFrame {
                         f.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
                     }
                     if(P1.getCase(i,j)==8) {
-                        f.setColor(Color.BLACK);
+                        f.setColor(Color.darkGray);
                         f.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
                     }
                     if(P1.getCase(i,j)==9) {
@@ -764,50 +798,123 @@ public class GameGUI extends javax.swing.JFrame {
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 15;j++) {
                 if( P2.getCase(i,j) >= 0){
-                    if( P2.getCase(i,j) >= 0){
+
                         if(P2.getCase(i,j)==0) {
-                            g.setColor(Color.orange);
-                            g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            if (m_coor.getTouche() && m_coor.getNavire() == 0) {
+                                g.setColor(Color.BLACK);
+                                g.fillRect((sx2 * 26) + 1, (sy2 * 26) + 1, 24, 24);
+                                PV0 = PV0+1;
+                            } else if(PV0 ==0) {
+                                g.setColor(Color.orange);
+                                g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            }
                         }
                         if(P2.getCase(i,j)==1) {
-                            g.setColor(Color.GREEN);
-                            g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            if (m_coor.getTouche() && m_coor.getNavire() == 1) {
+                                g.setColor(Color.BLACK);
+                                g.fillRect((sx2 * 26) + 1, (sy2 * 26) + 1, 24, 24);
+                                PV1 = PV1+1;
+                            } else if(PV1 ==0) {
+                                g.setColor(Color.GREEN);
+                                g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            }
                         }
                         if(P2.getCase(i,j)==2) {
-                            g.setColor(Color.BLUE);
-                            g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            if (m_coor.getTouche() && m_coor.getNavire() == 2) {
+                                g.setColor(Color.BLACK);
+                                g.fillRect((sx2 * 26) + 1, (sy2 * 26) + 1, 24, 24);
+                                PV2 = PV2+1;
+                            } else if(PV2 ==0) {
+                                g.setColor(Color.BLUE);
+                                g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            }
                         }
                         if(P2.getCase(i,j)==3) {
-                            g.setColor(Color.YELLOW);
-                            g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            System.out.println(m_coor.getTouche());
+                            if (m_coor.getTouche() && m_coor.getNavire() == 3) {
+                                g.setColor(Color.BLACK);
+                                g.fillRect((sx2 * 26) + 1, (sy2 * 26) + 1, 24, 24);
+                                PV3 = PV3+1;
+                            } else if(PV3 ==0) {
+                                g.setColor(Color.YELLOW);
+                                g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            }
                         }
                         if(P2.getCase(i,j)==4) {
-                            g.setColor(Color.RED);
-                            g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            if(m_coor.getTouche() && m_coor.getNavire()==4){
+                                g.setColor(Color.BLACK);
+                                g.fillRect((sx2 * 26) + 1, (sy2 * 26) + 1, 24, 24);
+                                PV4 = PV4+1;
+                            }
+                            else if(PV4 ==0) {
+                                g.setColor(Color.RED);
+                                g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            }
                         }
                         if(P2.getCase(i,j)==5) {
-                            g.setColor(Color.MAGENTA);
-                            g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            if (m_coor.getTouche() && m_coor.getNavire() == 5) {
+                                g.setColor(Color.BLACK);
+                                g.fillRect((sx2 * 26) + 1, (sy2 * 26) + 1, 24, 24);
+                                PV5 = PV5+1;
+                            } else if(PV5 ==0) {
+                                g.setColor(Color.MAGENTA);
+                                g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            }
                         }
                         if(P2.getCase(i,j)==6) {
-                            g.setColor(Color.PINK);
-                            g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            if (m_coor.getTouche() && m_coor.getNavire() == 6) {
+                                g.setColor(Color.BLACK);
+                                g.fillRect((sx2 * 26) + 1, (sy2 * 26) + 1, 24, 24);
+                                PV6 = PV6+1;
+                            } else if(PV6 ==0) {
+                                g.setColor(Color.PINK);
+                                g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            }
                         }
-                        if(P2.getCase(i,j)==7) {
-                            g.setColor(Color.CYAN);
-                            g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                    if(P2.getCase(i,j)==7) {
+                            if (m_coor.getTouche() && m_coor.getNavire() == 7)
+                            {
+                                g.setColor(Color.BLACK);
+                                g.fillRect((sx2 * 26) + 1, (sy2 * 26) + 1, 24, 24);
+                                PV7 = PV7+1;
+                            }
+                            else if(PV7 ==0)
+                            {
+                                g.setColor(Color.CYAN);
+                                g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            }
                         }
                         if(P2.getCase(i,j)==8) {
-                            g.setColor(Color.BLACK);
-                            g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+
+                            if(m_coor.getTouche() && m_coor.getNavire()==8){
+                                g.setColor(Color.BLACK);
+                                g.fillRect((sx2 * 26) + 1, (sy2 * 26) + 1, 24, 24);
+                                PV8 = PV8+1;
+                            }
+                            else if(PV8 ==0)
+                            {
+                                g.setColor(Color.DARK_GRAY);
+                                g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+
+                            }
                         }
                         if(P2.getCase(i,j)==9) {
-                            g.setColor(Color.LIGHT_GRAY);
-                            g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            if(m_coor.getTouche() && m_coor.getNavire()==9)
+                            {
+                                g.setColor(Color.BLACK);
+                                g.fillRect((sx2 * 26) + 1, (sy2 * 26) + 1, 24, 24);
+                                PV9 = PV9+1;
+
+                            }
+                            else if(PV9 ==0)
+                            {
+                                g.setColor(Color.LIGHT_GRAY);
+                                g.fillRect((i * 26) + 1, (j * 26) + 1, 24, 24);
+                            }
                         }
 
                     }
-                }
+
                 else{
                     g.clearRect((i*26)+1 ,(j *26)+1,  24, 24);
                 }
