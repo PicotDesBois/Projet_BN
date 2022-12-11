fed00b718a3b9556390907bf872d9bfa0d05a4ad
SlmMohamed7 <94547084+SlmMohamed7@users.noreply.github.com>
2022-12-11 17:53:37 +0100

    pages graphiques
    menu/ aide / defaite / victoire

---
+++ src/Vue/pages_graph/menu2.java
@@ -0,0 +1,52 @@
+package org.Package.pages_graph;
+
+import org.Package.GameGUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class menu2 extends JFrame{
+    private JButton nouvellePartieButton;
+    private JButton chargerUnePartieButton;
+    private JButton aideButton;
+    private JPanel Mainpanel;
+
+    public menu2() {
+        setLocation(250, 100);
+        setTitle("   Welcome   ");
+        setSize(800, 400);
+        setContentPane(Mainpanel);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        aideButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Aide().setVisible(true);
+                setVisible(false);
+            }
+        });
+        nouvellePartieButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new GameGUI().setVisible(true);
+                setVisible(false);
+            }
+        });
+    }
+    public static void main (String args[]) {
+
+        menu2 myFrame = new menu2();
+
+    }
+}
+
